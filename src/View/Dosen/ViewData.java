f8a2aa872186e6a84ac4a334ba1b5423cec371af
Imam Khusain <143572009+ImamKhusain@users.noreply.github.com>
2025-05-26 10:21:43 +0700

    Update ViewData.java

--- src/View/Dosen/ViewData.java
+++ src/View/Dosen/ViewData.java
@@ -25,7 +25,8 @@ public class ViewData extends JFrame {
     JButton tombolTambah = new JButton("Tambah Dosen");
     JButton tombolEdit = new JButton("Edit Dosen");
     JButton tombolHapus = new JButton("Hapus Dosen");
-
+    JButton tombolKembali = new JButton("Kembali ke menu utama");
+    
     /*
       Untuk membuat Tabel, kita memerlukan 3 komponen, yaitu:
       1. JTable sebagai komponen tabelnya
@@ -52,20 +53,22 @@ public class ViewData extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
-        setSize(552, 540);
+        setSize(552, 640);
 
         add(header);
         add(scrollPane);
         add(tombolTambah);
         add(tombolEdit);
         add(tombolHapus);
+        add(tombolKembali);
 
         header.setBounds(20, 8, 440, 24);
         scrollPane.setBounds(20, 36, 512, 320);
         tombolTambah.setBounds(20, 370, 512, 40);
         tombolEdit.setBounds(20, 414, 512, 40);
         tombolHapus.setBounds(20, 456, 512, 40);
-
+        tombolKembali.setBounds(20, 502, 512, 40);
+        
         /*
           Memanggil method showData() dari controller untuk
           mengisi tabel dengan data yang diambil dari DB
@@ -151,6 +154,14 @@ public class ViewData extends JFrame {
                 } else {
                     JOptionPane.showMessageDialog(null, "Data belum dipilih.");
                 }
+            }
+        });
+        
+        tombolKembali.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new View.HomePage().setVisible(true);
             }
         });
     }
